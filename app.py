e0135fa42cfbe60bc45546aeecebdce7663b32da
Victor Mutuku <vkiokomutuku@gmail.com>
2026-02-09 15:25:04 +0300

    Fix the app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ st.title("California housing price prediction")
 st.write("Enter the housing features below")
 
 #Input fields(same order as datasets)
-MedInc=st.number_input("median income",value=3.0)
-HouseAge = st.number_input("House Age", value=20.0)
-AveRooms = st.number_input("Average Rooms", value=5.0)
-AveBedrms = st.number_input("Average Bedrooms", value=1.0)
-Population = st.number_input("Population", value=1000.0)
-AveOccup = st.number_input("Average Occupancy", value=3.0)
-Latitude = st.number_input("Latitude", value=34.0)
-Longitude = st.number_input("Longitude", value=-118.0)
+MedInc = st.number_input("Median Income (10k $)", value=5.0, min_value=1.0, max_value=15.0, step=0.1)
+HouseAge = st.number_input("House Age (years)", value=20.0, min_value=1.0, max_value=52.0, step=1.0)
+AveRooms = st.number_input("Average Rooms", value=6.0, min_value=2.0, max_value=10.0, step=0.1)
+AveBedrms = st.number_input("Average Bedrooms", value=2.0, min_value=1.0, max_value=5.0, step=0.1)
+Population = st.number_input("Population", value=1000, min_value=3, max_value=5000, step=1)
+AveOccup = st.number_input("Average Occupancy", value=3.0, min_value=1.0, max_value=5.0, step=0.1)
+Latitude = st.number_input("Latitude", value=34.0, min_value=32.0, max_value=42.0, step=0.01)
+Longitude = st.number_input("Longitude", value=-118.0, min_value=-124.0, max_value=-114.0, step=0.01)
 
 if st.button("predict price"):
   input_df=pd.DataFrame([[MedInc,HouseAge,AveRooms,AveBedrms,Population,AveOccup,Latitude,Longitude]],
